90267bebe426635dfc054010551b065ce12ca108
kalyan7333 <144237510+kalyan7333@users.noreply.github.com>
2025-09-05 17:16:15 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -4,7 +4,7 @@ on AWS provider
 */
 resource "aws_instance" "web" {
   ami                         = "ami-020cba7c55df1f615" #Ubuntu 20.04 LTS Free Tier Image
-  instance_type               = "t3.micro"
+  instance_type               = "t3.small"
   key_name                    = "terraform_demo"
   subnet_id                   = aws_subnet.public_subnet.id
   vpc_security_group_ids      = [aws_security_group.kalyan_sg.id]
